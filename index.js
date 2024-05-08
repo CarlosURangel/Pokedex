896b7307ca62d9fd52fba17f3dcd8e9974964898
CarlosURangel <crangel30@alumnos.uaq.mx>
2024-05-08 07:49:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) =>{
+    console.log(req);
+    res.send("Hola Mundo");
+});
+
+app.get("/pokemon", (req, res, next) =>{
+    res.json({ nombre: "Ulises", edad: "19"});
+});
+app.listen(3000, () =>{
+    console.log("Server is running");
+});
